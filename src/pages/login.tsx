fcd78d05a0c265f5565562640dff1d68aa97bc84
riot1806 <riot180618@gmail.com>
2023-08-30 08:34:43 +0500

    third

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,17 @@
 import styles from '@/styles/Login.module.scss';
 
+import { useRouter } from 'next/router';
+
+import { useGetLS } from '@/hooks/ls';
 import SecLayout from '@/components/layout/SecLayout';
 import LoginModal from '@/components/modal/login/LoginModal';
 
 const Login = () => {
+  const isAuth = useGetLS('token');
+  const router = useRouter();
+
+  if (isAuth) router.push('/profile');
+
   return (
     <SecLayout title='ЛОГИН'>
       <div className={styles.login}>
--- src/pages/reg.tsx
+++ src/pages/reg.tsx
@@ -1,9 +1,17 @@
 import styles from '@/styles/Login.module.scss';
 
+import { useRouter } from 'next/router';
+
+import { useGetLS } from '@/hooks/ls';
 import SecLayout from '@/components/layout/SecLayout';
 import RegModal from '@/components/modal/reg/RegModal';
 
 const Reg = () => {
+  const isAuth = useGetLS('token');
+  const router = useRouter();
+
+  if (isAuth) return router.push('/profile');
+
   return (
     <SecLayout title='РЕГИСТРАЦИЯ'>
       <div className={styles.reg}>
